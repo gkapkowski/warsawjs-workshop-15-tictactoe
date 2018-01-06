ee60c5e72dac68c029e9891314f2dd3ec055eaee
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2018-01-06 13:45:14 +0100

    Add click and players

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-console.log('działa');
+document.addEventListener('DOMContentLoaded', () => {
+    var playerClasses = ['red', 'blue'];
+    var currentPlayer = 0;
+
+    var fields = document.querySelectorAll('.board > div');
+
+    function connectField (element) {
+        element.addEventListener('click', function fieldClickHandler () {
+
+            var playerClass = playerClasses[currentPlayer];
+
+            element.classList.add(playerClass);
+            currentPlayer = currentPlayer ? 0 : 1;
+
+
+            element.removeEventListener('click', fieldClickHandler);
+        })
+    }
+
+    fields.forEach(connectField);
+})
