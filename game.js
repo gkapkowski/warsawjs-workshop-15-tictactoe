779c67473430a9a7e19284c3a099596c800bd7d6
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2018-01-06 14:51:51 +0100

    Working game

--- game.js
+++ game.js
@@ -18,48 +18,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
             fields = document.querySelectorAll('.board > div');
 
-            redBoard = '';
-            fields.forEach(element => {
-                redBoard+= element.classList.contains('red') ? 'R' : '-';
-            });
+            //3 Poziome
+            var row1 = fields[0].className + fields[1].className + fields[2].className;
+            var row2 = fields[3].className + fields[4].className + fields[5].className;
+            var row3 = fields[6].className + fields[7].className + fields[8].className;
 
-            console.log(redBoard);
+            //3 Pionowe
+            var column1 = fields[0].className + fields[3].className + fields[6].className;
+            var column2 = fields[1].className + fields[4].className + fields[7].className;
+            var column3 = fields[2].className + fields[5].className + fields[8].className;
 
-            switch (redBoard) {
-                case 'RRR------':
-                case '---RRR---':
-                case '------RRR':
-                case 'R--R--R--':
-                case '-R--R--R-':
-                case '--R--R--R':
-                case 'R---R---R':
-                case '--R-R-R--':
-                    alert('Red win!');
+            //2 Skosy
+            var digonal1 = fields[0].className + fields[4].className + fields[8].className;
+            var digonal2 = fields[6].className + fields[4].className + fields[2].className;
+            
+            if (row1 === 'redredred' ||
+                row2 === 'redredred' ||
+                row3 === 'redredred' ||
+                column1 === 'redredred' ||
+                column2 === 'redredred' ||
+                column3 === 'redredred' ||
+                digonal1 === 'redredred' ||
+                digonal2 === 'redredred') {
+                    alert("Red Win!");
                     resetGame();
-                    break;
-            }
+                }
 
-            blueBoard = '';
-            fields.forEach(element => {
-                blueBoard+= element.classList.contains('blue') ? 'B' : '-';
-            });
-
-            console.log(blueBoard);
-
-            switch (blueBoard) {
-                case 'BBB------':
-                case '---BBB---':
-                case '------BBB':
-                case 'B--B--B--':
-                case '-B--B--B-':
-                case '--B--B--B':
-                case 'B---B---B':
-                case '--B-B-B--':
-                    alert('Blue win!');
-					resetGame();
-                    break;
-            }
-
+            if (row1 === 'blueblueblue' ||
+                row2 === 'blueblueblue' ||
+                row3 === 'blueblueblue' ||
+                column1 === 'blueblueblue' ||
+                column2 === 'blueblueblue' ||
+                column3 === 'blueblueblue' ||
+                digonal1 === 'blueblueblue' ||
+                digonal2 === 'blueblueblue') {
+                    alert("Blue Win!");
+                    resetGame();
+                }
+            
             // game over ?
 			if(filledFields === 9) {
 				alert('Tie');
@@ -74,7 +70,8 @@ document.addEventListener('DOMContentLoaded', () => {
 			field.removeAttribute('class');
 			connectField(field);
 		});
-		counter = 0;
+        filledFields = 0;
+        currentPlayer = 0;
 	}
 
     fields.forEach(connectField);
